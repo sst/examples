7ccc8122c211e409c75cf9fd9c68e39356350acc
Jay <jvelayud@gmail.com>
2021-03-27 20:26:17 -0400

    Fixing typo

--- graphql-apollo/lib/MyStack.ts
+++ graphql-apollo/lib/MyStack.ts
@@ -4,7 +4,7 @@ export default class MyStack extends sst.Stack {
   constructor(scope: sst.App, id: string, props?: sst.StackProps) {
     super(scope, id, props);
 
-    // Create the Apollo GrapQL API
+    // Create the Apollo GraphQL API
     const api = new sst.ApolloApi(this, "ApolloApi", {
       server: "src/lambda.handler",
     });
