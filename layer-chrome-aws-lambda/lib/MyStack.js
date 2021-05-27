c738fa12933442d92f39d2b0e25df4a01c887347
Jay <jvelayud@gmail.com>
2021-05-27 14:21:05 -0400

    Updating layer example

--- layer-chrome-aws-lambda/lib/MyStack.js
+++ layer-chrome-aws-lambda/lib/MyStack.js
@@ -1,48 +1,35 @@
-import * as s3 from "@aws-cdk/aws-s3";
-import * as lambda from "@aws-cdk/aws-lambda";
+import { LayerVersion } from "@aws-cdk/aws-lambda";
 import * as sst from "@serverless-stack/resources";
+
+const layerArn =
+  "arn:aws:lambda:us-east-1:764866452798:layer:chrome-aws-lambda:22";
 
 export default class MyStack extends sst.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    // Create S3 bucket
-    const bucket = new s3.Bucket(this, "MyBucket", {
-      publicReadAccess: true,
-    });
+    const layer = LayerVersion.fromLayerVersionArn(this, "Layer", layerArn);
 
-    // Get Chrome Layer
-    const layerArn =
-      "arn:aws:lambda:us-east-1:764866452798:layer:chrome-aws-lambda:22";
-    const layer = lambda.LayerVersion.fromLayerVersionArn(
-      this,
-      "Layer",
-      layerArn
-    );
-
-    // Create Api
+    // Create a HTTP API
     const api = new sst.Api(this, "Api", {
       routes: {
         "GET /": {
           function: {
-            handler: "src/lambda.main",
-            bundle: {
-              externalModules: ["chrome-aws-lambda"],
-            },
-            environment: {
-              BUCKET_NAME: bucket.bucketName,
-            },
+            handler: "src/lambda.handler",
+            // Increase the timeout for generating screenshots
+            timeout: 15,
+            // Load Chrome in a Layer
             layers: [layer],
+            // Exclude bundling it in the Lambda function
+            bundle: { externalModules: ["chrome-aws-lambda"] },
           },
         },
       },
     });
-    api.attachPermissions([bucket]);
 
-    // Show the API endpoint and other info in the output
+    // Show the endpoint in the output
     this.addOutputs({
-      ApiEndpoint: api.httpApi.apiEndpoint,
-      BucketName: bucket.bucketName,
+      ApiEndpoint: api.url,
     });
   }
 }
